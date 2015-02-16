39d017984567312ce6cea207fc256c00c0404643
kwdkwd <kwd@dsecargo.com>
2015-02-16 14:33:07 +0900

    aaa

--- WindowsFormsApplication6/Form1.cs
+++ WindowsFormsApplication6/Form1.cs
@@ -20,6 +20,7 @@ namespace WindowsFormsApplication6
         private void Test_button_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello, " + DateTime.Now.ToString());
+            MessageBox.Show("추가 메세지");
         }
     }
 }
